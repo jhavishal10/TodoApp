dd8ca76a1392e690ce87b28439caf649cf0a686a
_vishal_jha <vishal.jha@indwealth.in>
2020-05-30 17:51:30 +0530

    new task Ui added

--- app/src/main/java/android/com/urbanclapassignment/ui/HomeActivity.kt
+++ app/src/main/java/android/com/urbanclapassignment/ui/HomeActivity.kt
@@ -3,54 +3,45 @@ package android.com.urbanclapassignment.ui
 import android.com.urbanclapassignment.R
 import android.com.urbanclapassignment.model.ListItem
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.add_new_task_layout.view.*
+
 
 class HomeActivity : AppCompatActivity() {
     private val adapter = TasksAdapter()
-//    private lateinit var viewModel: ViewModel
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-//        viewModel = ViewModelProviders.of(this).get(ViewModel::class.java)
-//
-//        viewModel.users.observe(this, Observer { tasks: List<ListItem> ->
-//            adapter.addData(tasks)
-//
-//        })
         initUi()
     }
 
     private fun initUi() {
+        addNewTaskButton.debouncedOnClick {
+            val builder = AlertDialog.Builder(this)
+            val mView: View = layoutInflater.inflate(R.layout.add_new_task_layout, null)
+            builder.setView(mView)
+            val alertDialog: AlertDialog = builder.create()
+            alertDialog.setCancelable(false)
+            alertDialog.show()
+            mView.addTaskButton.debouncedOnClick {
+                alertDialog.dismiss()
+                Toast.makeText(
+                    applicationContext,
+                    mView.taskText.text.toString().substring(0, 20) + "........Added",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+        }
         val task = mutableListOf<ListItem>()
-        val listItem1 =ListItem("asrdytubjnobhgcftygvhbigvcfdtxrtcgvubiyvcdxrsdfybiunbyvtcxrsdcybuivtcrdxcfvgybuitvcxrsdcfvgbyvxsezxdcfvgbuhigvxsezxdcfvgbuhsxedcgbuhnbgxsedcgbuhnbfcdxsedcfvgbuhnijhbgvxsdf",true)
-        val listItem2 =ListItem("asfcgvhjbkdf",true)
-        val listItem3 =ListItem("afcygvhetdrxcyvuybhijnhvgcfxdtzsrdxtfcygvbhjnbhvgcfxdzsdxfcgvhbjhvgcfxdfzsdxcgvhbjnobuyvutcrxtezrsxtrcyuvihoijhugyfutydrtezrtxryctugiuhogiyftudyrteszrtrytugiuhogiyftdyrstezrwtxrytfuygiuyftudrystezrtrxdytfuygiuhogiyfutdyrsterzwbjksdf",false)
-        val listItem4 =ListItem("gycvhbjn",true)
-        task.add(listItem1)
-        task.add(listItem2)
-        task.add(listItem3)
-        task.add(listItem4)
-        task.add(listItem4)
-        task.add(listItem4)
-        task.add(listItem4)
-        task.add(listItem4)
-        task.add(listItem4)
-        task.add(listItem4)
-        task.add(listItem4)
-        task.add(listItem4)
-        task.add(listItem4)
-        task.add(listItem4)
-        task.add(listItem4)
-        task.add(listItem4)
-        task.add(listItem4)
-        task.add(listItem4)
-        task.add(listItem4)
         adapter.addData(task)
         taskRecyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         taskRecyclerView.adapter = adapter
