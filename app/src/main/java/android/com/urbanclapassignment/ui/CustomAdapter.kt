87a5311a3e32d8844bd103ee86885540decd0847
_vishal_jha <vishal.jha@indwealth.in>
2020-05-31 01:32:28 +0530

    adapter fixes and code formatting

--- app/src/main/java/android/com/urbanclapassignment/ui/CustomAdapter.kt
+++ app/src/main/java/android/com/urbanclapassignment/ui/CustomAdapter.kt
@@ -11,12 +11,18 @@ import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.list_item.view.*
 
-class TasksAdapter(private val adapterCallbackInterface: AdapterCallbackInterface) : RecyclerView.Adapter<TasksAdapter.IpoCompanyBusinessViewHolder>() {
+class TasksAdapter(private val adapterCallbackInterface: AdapterCallbackInterface) :
+    RecyclerView.Adapter<TasksAdapter.IpoCompanyBusinessViewHolder>() {
 
     private val list = mutableListOf<ListItem>()
 
     fun addData(list: List<ListItem>) {
         this.list.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    fun clearItems() {
+        this.list.clear()
         notifyDataSetChanged()
     }
 
@@ -29,46 +35,47 @@ class TasksAdapter(private val adapterCallbackInterface: AdapterCallbackInterfac
         viewType: Int
     ): IpoCompanyBusinessViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
-        return IpoCompanyBusinessViewHolder(view,adapterCallbackInterface)
+        return IpoCompanyBusinessViewHolder(view, adapterCallbackInterface)
 
     }
 
-    inner class IpoCompanyBusinessViewHolder(view: View, callback: AdapterCallbackInterface) : RecyclerView.ViewHolder(view) {
+    inner class IpoCompanyBusinessViewHolder(view: View, callback: AdapterCallbackInterface) :
+        RecyclerView.ViewHolder(view) {
 
         init {
             itemView.deleteButton.debouncedOnClick {
-                val item = it.tag as ListItem
-                    callback.deleteClicked(item.taskId)
+                val item = itemView.tag as ListItem
+                callback.deleteClicked(item)
             }
             itemView.undoButton.debouncedOnClick {
-                val item = it.tag as ListItem
-                callback.undoClicked(item.taskId)
+                val item = itemView.tag as ListItem
+                callback.undoClicked(item)
             }
             itemView.markDoneButton.debouncedOnClick {
-                val item = it.tag as ListItem
-                callback.markDoneClicked(item.taskId)
+                val item = itemView.tag as ListItem
+                callback.markDoneClicked(item,position)
             }
             itemView.debouncedOnClick {
-                val item = it.tag as ListItem
-                callback.taskClicked(item.taskId)
+                val item = itemView.tag as ListItem
+                callback.taskClicked(item)
             }
         }
 
         fun bind(item: ListItem) {
             with(itemView) {
                 tag = item
-                if (item.done) {
+                if (item.done == 1) {
                     taskName.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
                     undoButton.isVisible = true
                     deleteButton.isVisible = true
                     markDoneButton.isVisible = false
                 } else {
+                    taskName.paintFlags = Paint.ANTI_ALIAS_FLAG
                     undoButton.isVisible = false
                     deleteButton.isVisible = false
                     markDoneButton.isVisible = true
                 }
                 taskName.text = item.taskName
-
             }
         }
     }
@@ -77,12 +84,13 @@ class TasksAdapter(private val adapterCallbackInterface: AdapterCallbackInterfac
         holder.bind(list[position])
     }
 }
-    interface AdapterCallbackInterface {
-        fun undoClicked(id: Int)
-        fun deleteClicked(id: Int)
-        fun markDoneClicked(id: Int)
-        fun taskClicked(id: Int)
-    }
+
+interface AdapterCallbackInterface {
+    fun undoClicked(task:ListItem)
+    fun deleteClicked(task:ListItem)
+    fun markDoneClicked(task:ListItem,pos:Int)
+    fun taskClicked(task:ListItem)
+}
 
 inline fun View.debouncedOnClick(debounceTill: Long = 500, crossinline onClick: (v: View) -> Unit) {
     this.setOnClickListener(object : DebouncedOnClickListener(debounceTill) {
